fbe8a58e8b5f136fcc95fb7647ac150be5b63dbe
Katsuhiro Yamauchi <katsu.ymw@gmail.com>
2020-04-01 12:44:56 +0900

    basic認証を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
 before_action :basic_auth if: :production?
 
   private
+  def production?
+    Rails.env.production?
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
