efca06f2e1afdabd2bda4525780748cc8a63cd15
kaito0830 <kaitobad830@outlook.jp>
2020-04-02 17:30:23 +0900

    revise routing of items create to new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:create]
+  resources :items, only: [:new]
   resources :users, only: [:show]
 end
