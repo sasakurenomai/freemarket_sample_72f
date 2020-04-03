a64df84393da09d00e37758eabc6ab11bda8f944
okaday54 <okaday2020@gmail.com>
2020-04-03 16:28:15 +0900

    ご指摘分の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  validates_format_of :password, :with => /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, :message => "７文字以上の半角英数字で入力してください。"
+  validates_format_of :password, with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, message: "７文字以上の半角英数字で入力してください。"
 end
