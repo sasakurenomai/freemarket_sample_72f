b1c57129c84a8a61fd29ae30383048d291bf678a
Katsuhiro Yamauchi <katsu.ymw@gmail.com>
2020-04-06 21:12:08 +0900

    items#showを有効化

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:new]
-  get 'items/show'
+  resources :items, only: [:new, :show]
   resources :users, only: [:show]
 end
