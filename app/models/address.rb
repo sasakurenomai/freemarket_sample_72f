12c06d5ce7c97699209d14aff1478c59396a8e7e
okaday54 <okaday2020@gmail.com>
2020-04-07 15:29:17 +0900

    単体テスト追記(Valid)

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
-  # validates :shipping_first_name, :shipping_last_name, :shipping_first_name_kana, :shipping_last_name_kana,
-  # :zipcode, :prefecture, :city, :street ,presence: true
-  
+  validates :shipping_first_name, :shipping_last_name, :shipping_first_name_kana, :shipping_last_name_kana,
+  :zipcode, :prefecture, :city, :street ,presence: true
 end
 
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe User do
+  describe '#create' do
+    it " nicknameがない場合は登録できないこと" do
+      user = build(:user, nickname: nil)
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
+
+    it "emailがない場合は登録できないこと" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "passwordがない場合は登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "passwordが存在してもpassword_confirmationがない場合は登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it " passwordが7文字以上であれば登録できること " do
+      user = build(:user, password: "0000000", password_confirmation: "0000000")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    it " passwordが6文字以下であれば登録できないこと " do
+      user = build(:user, password: "000000", password_confirmation: "000000")
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 7 characters)")
+    end
+  end
+end
