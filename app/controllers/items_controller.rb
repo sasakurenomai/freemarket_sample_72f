9b08340ea4cebc2c7ffe4aab8ebbc50f1a200007
Katsuhiro Yamauchi <katsu.ymw@gmail.com>
2020-04-07 23:38:13 +0900

    Items, Usersテーブルの内容を表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,9 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  def show
+    @item = Item.find(params[:id])
+    @seller = User.find(@item.users_items.find_by(item_id: @item.id).seller_id)
+  end
 end
