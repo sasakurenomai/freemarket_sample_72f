e4c693bb8ad9e47a380b20dd40a6943416fddfcb
Katsuhiro Yamauchi <katsu.ymw@gmail.com>
2020-04-08 14:16:50 +0900

    バリデーションの設定とオプションdependent: :destroyの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
+    # "validates :カラム名, presence: true" を適用するカラム
+    columns = [:name, :details, :user_id, :sales_status, :item_status, :price, :shipping_area, :shipping_cost, :shipping_bays]
+
+    columns.each do |column|
+        validates column, presence: true
+    end
+
     belongs_to :user
-    has_many :item_images
+    has_many :item_images, dependent: :destroy
     # belongs_to :category
 end
--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,3 +1,6 @@
 class ItemImage < ApplicationRecord
+  validates :image_url, presence: true
+  validates :item_id,   presence: true
+
   belongs_to :item
 end
