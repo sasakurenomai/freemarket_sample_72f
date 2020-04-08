5dbd2edead8f06b3670eef4d3e7e4013df35bbd1
Katsuhiro Yamauchi <katsu.ymw@gmail.com>
2020-04-08 14:27:10 +0900

    DB設計見直しにより変更されたを適用
    Users_ItemsテーブルがなくなりItemsテーブルに統合された事でUsersとItemsテーブル間でのアソシエーションが正常に機能するようになりました。
    Users_Itemsテーブルでseller_idだったものは、Itemsテーブルではuser_idとして表記されています。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,5 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @seller = User.find(@item.users_items.find_by(item_id: @item.id).seller_id)
   end
 end
