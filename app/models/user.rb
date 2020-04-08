5645c9d1c4bbf9a148ceaf8c39701c1bdf3067d2
Katsuhiro Yamauchi <katsu.ymw@gmail.com>
2020-04-08 14:04:22 +0900

    アソシエーションにオプションdependent: :destroyを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   validates_format_of :password, with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, message: "７文字以上の半角英数字で入力してください。"
   validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday ,presence: true
   has_one :address
-  has_many :items
+  has_many :items, dependent: :destroy
 end
 
