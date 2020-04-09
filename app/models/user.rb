2f2bb3543d40cdf167aae6315491d5139a9b713f
okaday54 <okaday2020@gmail.com>
2020-04-10 00:41:04 +0900

    コメント機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ApplicationRecord
   validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday ,presence: true
   has_one :address
   has_many :items, dependent: :destroy
+  has_many :comments
 end
 
