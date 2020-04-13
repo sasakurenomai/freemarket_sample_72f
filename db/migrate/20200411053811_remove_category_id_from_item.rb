f9f40322e63f57f58ccefc1f628162d63dee8596
kaito0830 <kaitobad830@outlook.jp>
2020-04-13 20:52:05 +0900

    マイグレーションファイルの修正

--- db/migrate/20200411053811_remove_category_id_from_item.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCategoryIdFromItem < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :items, :category_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_013546) do
+ActiveRecord::Schema.define(version: 2020_04_13_115003) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
