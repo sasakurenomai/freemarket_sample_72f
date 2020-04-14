47fa65d92b0834feb551607571a927a689f08bea
Katsuhiro Yamauchi <katsu.ymw@gmail.com>
2020-04-14 17:01:14 +0900

    バリデーション設定をコメントアウト

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,8 +1,8 @@
 class ItemImage < ApplicationRecord
   mount_uploader :image_url , ImageUploader
   
-  validates :image_url, presence: true
-  validates :item_id,   presence: true
-  
+  # validates :image_url, presence: true
+  # validates :item_id,   presence: true
+
   belongs_to :item
 end
