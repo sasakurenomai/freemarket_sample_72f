7a4b928d63ac8cc61a9881d710fed6c79462897a
Katsuhiro Yamauchi <katsu.ymw@gmail.com>
2020-04-18 15:38:23 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 2020_04_16_082854) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "ancestry"
@@ -110,5 +119,6 @@ ActiveRecord::Schema.define(version: 2020_04_16_082854) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "users"
+  add_foreign_key "cards", "users"
   add_foreign_key "item_images", "items"
 end
