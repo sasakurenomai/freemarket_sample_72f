5fcf384bddaf8638d8563f6a406a59f0e64158b6
kaito0830 <kaitobad830@outlook.jp>
2020-04-22 13:17:22 +0900

    編集後のルート先変更など細かな点修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -44,4 +44,49 @@ describe Item do
       expect(item.errors[:shipping_days_id]).to include("を入力してください")
     end
   end
+
+  describe '#edit' do
+
+    it "nameがない場合は出品できないこと" do
+      item = Item.new()
+      item.valid?
+      expect(item.errors.messages[:name]).to include("を入力してください")
+    end
+
+    it "detailsがない場合は出品できないこと" do
+      item = build(:item, details: "")
+      item.valid?
+      expect(item.errors[:details]).to include("を入力してください")
+    end
+
+    it "priceがない場合は出品できないこと" do
+      item = build(:item, price: "")
+      item.valid?
+      expect(item.errors[:price]).to include("を入力してください")
+    end
+
+    it "user_idがない場合は出品できないこと" do
+      item = build(:item, user_id: "")
+      item.valid?
+      expect(item.errors[:user_id]).to include("を入力してください")
+    end
+
+    it "item_status_idがない場合は出品できないこと" do
+      item = build(:item, item_status_id: "")
+      item.valid?
+      expect(item.errors[:item_status_id]).to include("を入力してください")
+    end
+
+    it "shipping_area_idがない場合は出品できないこと" do
+      item = build(:item, shipping_area_id: "")
+      item.valid?
+      expect(item.errors[:shipping_area_id]).to include("を入力してください")
+    end
+
+    it "shipping_days_idがない場合は出品できないこと" do
+      item = build(:item, shipping_days_id: "")
+      item.valid?
+      expect(item.errors[:shipping_days_id]).to include("を入力してください")
+    end
+  end
 end
