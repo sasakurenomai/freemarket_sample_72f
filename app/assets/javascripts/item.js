ca8597ffe71461c945a160af73f5c68878a81776
kaito0830 <kaitobad830@outlook.jp>
2020-04-23 17:38:18 +0900

    出品・編集機能修正

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -3,16 +3,25 @@ $(function(){
     const html = `<div data-index="${index}" class="js-file_group">
                     <input class="js-file" type="file"
                     name="item[item_images_attributes][${index}][image_url]"
-                    id="item_item_images_attributes_${index}_image_url image-file"><br>
+                    id="item_item_images_attributes_${index}_id"><br>
                     <div class="js-remove">削除</div>
                   </div>`;
     return html;
   }
 
+  const buildFileFields = (index)=> {
+    const html = `<div data-index="${index}" class="js-file_group">
+                    <input class="js-files" type="file"
+                    name="item[item_images_attributes][${index}][image_url]"
+                    id="item_item_images_attributes_${index}_id"><br>
+                    <div class="js-removes">削除</div>
+                  </div>`;
+    return html;
+  }
   const buildImg = (index, url) => {
     const html = `<div class="item_image">
     <img data-index="${index}" src="${url}" width="100px" height="100px">
-      <div class="js-remove">削除</div>
+      <div class="js-remove2">削除</div>
     </div>`;
     return html;
   }
@@ -27,6 +36,16 @@ $(function(){
     $('#image-box').append(buildFileField(fileIndex[0]));
     fileIndex.shift();
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+    $('.label-box').attr({id: `label-box--${new_id}`,for: `item_item_images_attributes_${new_id}_id`});
+  });
+
+  $('#image-box').on('change', '.js-files', function(e){
+    $('#image-box').append(buildFileFields(fileIndex[0]));
+    fileIndex.shift();
+    fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+    var id = $(this).attr('id').replace(/[^0-9]/g, '');
+    var new_id = parseInt(id) + 1;
+    $('.label-box').attr({id: `label-box--${new_id}`,for: `item_item_images_attributes_${new_id}_id`});
   });
 
   $('#image-box').on('click', '.js-remove', function(){
@@ -38,7 +57,16 @@ $(function(){
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
 
-  $('#image-box').on('change', '.js-file', function(e){
+  $('#image-box').on('click', '.js-remove2', function(){
+    const targetIndex = $(this).parent().data('index')
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    if (hiddenCheck){ hiddenCheck.prop('checked', true);
+    $(this).parent().remove();
+    $(`img[data-index="${targetIndex}"]`).remove();}
+    if ($('.js-files').length == 0) $('#image-box').append(buildFileFields(fileIndex[0]));
+  });
+
+  $('#image-box').on('change', '.js-files', function(e){
     const targetIndex = $(this).parent().data('index');
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
@@ -47,8 +75,6 @@ $(function(){
       img.setAttribute('src', blobUrl);
     }else{
       $('#previews').append(buildImg(targetIndex, blobUrl));
-      fileIndex.shift();
-      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
     }
   });
 
@@ -58,6 +84,6 @@ $(function(){
     if (hiddenCheck){ hiddenCheck.prop('checked', true);
     $(this).parent().remove();
     $(`img[data-index="${targetIndex}"]`).remove();}
-    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+    if ($('.js-file').length == 0) $('#image-box').append(buildFileFields(fileIndex[0]));
   });
 });
